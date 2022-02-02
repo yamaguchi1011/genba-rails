f6ba9e71de0d56f4c7d3ac2d2a1fa56bcc8f7060
yamaguchi <yamaguchi1001@icloud.com>
2022-02-02 21:36:38 +0900

    add admin user Chapter4 end

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc)}
+  private
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
