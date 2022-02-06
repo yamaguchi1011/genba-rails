2c41adf972fc6740e6313badd09969ab0622db50
yamaguchi <yamaguchi1001@icloud.com>
2022-02-06 22:50:58 +0900

    add confirm_new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,6 +33,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
@@ -41,6 +46,11 @@ class TasksController < ApplicationController
     end
   end
   
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
 
   private
   def task_params
